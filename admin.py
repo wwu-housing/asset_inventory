6c97d5184875c6a072ab3f4e38e3ba2ce0c24d10
Firass Asad <Firass.Asad@wwu.edu>
2011-11-01 10:25:53 -0700

    add device admin class and a list display for fields

--- admin.py
+++ admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from models import Location, Job, Person, Status, Esign, Esign_Sys, Purchase, Device 
+from models import Location, Job, Person, Status, Esign, Esign_Sys, Purchase, Device
 
-admin.site.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("hostname", "serial", "state_id", "teamviewer_ID", "ram",
+                    "dualscreen", "brand", "model", "proc", "x64",
+                    "purchase_ID", "Location_ID", "Person_ID", "Status_ID",
+                    "last_update", "is_deleted")
+admin.site.register(Device, DeviceAdmin)
 admin.site.register(Location)
 admin.site.register(Job)
 admin.site.register(Person)
