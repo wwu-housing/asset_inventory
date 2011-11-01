668bc3718df46280a74153426949f251f8639297
Firass Asad <Firass.Asad@wwu.edu>
2011-11-01 10:47:24 -0700

    add station location to admin list display

--- admin.py
+++ admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from models import Location, Job, Person, Status, Esign, Esign_Sys, Purchase, Device
 
+
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ("hostname", "serial", "state_id", "teamviewer_ID", "ram",
                     "dualscreen", "brand", "model", "proc", "x64",
-                    "purchase_ID", "Location_ID", "Person_ID", "Status_ID",
-                    "last_update", "is_deleted")
+                    "purchase_ID", "Location_ID", "station_location",
+                    "Person_ID", "Status_ID", "last_update", "is_deleted")
+
+    def station_location(self, obj):
+        return obj.Location_ID.station_location
 admin.site.register(Device, DeviceAdmin)
+
+
 admin.site.register(Location)
 admin.site.register(Job)
 admin.site.register(Person)
